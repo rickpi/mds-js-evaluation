ada1a24497bedf4f5edfe13d179682ebfb422840
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-09-26 18:18:12 +0200

    feat(style): method to style the app

--- exercise-3/index.js
+++ exercise-3/index.js
@@ -14,11 +14,26 @@ var MyMorpionXO = function MyMorpionXO() {
 };
 
 /**
+ * Style
+ * @return {Object} this
+ */
+MyMorpionXO.prototype._style = function() {
+	var sheet = document.createElement('style');
+
+	sheet.innerHTML = ':root { --primary-color: #9FD17A; --primary-color-light: #C7FE9F; --primary-color-dark: #8AD058; --white: #FFF; }';
+	sheet.innerHTML += 'html, body { padding: 0; margin: 0; box-sizing: border-box; font-size: 16px; }';
+	sheet.innerHTML += 'body { min-width: 100vw; min-height: 100vh; background-image: linear-gradient(90deg, var(--primary-color-light), var(--primary-color-dark)); display: flex; flex-direction: column; }';
+	sheet.innerHTML += '.container { margin: auto; width: 30em; height: 30em; padding: 2em; box-shadow: 0 0.8em 1em rgba(0, 0, 0, 0.65); background-color: var(--white); border-radius: 0.6em; }';
+	document.body.appendChild(sheet);
+	return this;
+};
+
+/**
  * Run
  * @return {Object} this
  */
 MyMorpionXO.prototype.run = function() {
-	console.log('run');
+	this._style();
 	return this;
 };
 
