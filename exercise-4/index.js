d7eed1fcda8268fe041031571a1b2e426b945148
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-09-28 09:18:03 +0200

    feat(WorldMap): add WorldMap class

---
+++ exercise-4/index.js
@@ -0,0 +1,20 @@
+/**
+ * index.js
+ */
+'use strict';
+
+/**
+ * WorldMap
+ * @constructor
+ */
+var WorldMap = function WorldMap() {
+	this.map;
+};
+
+/**
+ * Render World Map
+ * @return {Object} this
+ */
+WorldMap.prototype.renderWorldMap = function() {
+	return this;
+};
