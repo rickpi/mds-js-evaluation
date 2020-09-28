d44d82a94108ed8e314856b4f6b923830c7df9b6
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-09-29 00:09:09 +0200

    feat(updateCaption): add method displaying chosen country

--- exercise-4/index.js
+++ exercise-4/index.js
@@ -45,35 +45,72 @@ WorldMap.prototype._bindEvents = function() {
 
 	for (var i = 0; i < countries.length; i++) {
 		countries[i].addEventListener('mouseenter', function(e) {
-			var country = e.target;
-
-			if (country.id !== this.selected) {
-				country.style.fill = '#00F';	
-			}
-		});
+			this._hoverCountry(e.target, '#00F');
+		}.bind(this));
 		countries[i].addEventListener('mouseleave', function(e) {
-			var country = e.target;
-
-			if (country.id !== this.selected) {
-				country.style.fill = '#000';	
-			}
-		});
+			this._hoverCountry(e.target, '#000');
+		}.bind(this));
 		countries[i].addEventListener('click', function(e) {
-			var country = e.target;
-
-			if (country.id !== this.selected) {
-				this.selected = country.id;
-				country.style.fill = '#F00';
-				return ;
-			}
-
-			this.selected = null;
-			country.style.fill = '#000'
-		});
+			this._clickCountry(e.target);
+			this._updateCaption();
+		}.bind(this));
 	}
 
 	return this;
 };
+
+/**
+ * Hover Country
+ * @param {Object} country
+ * @param {string} color
+ * @return {Object} this
+ */
+WorldMap.prototype._hoverCountry = function(country, color) {
+	if (country.id !== this.selected) {
+		country.style.fill = color;	
+	}
+
+	return this;
+};
+
+/**
+ * Click Country
+ * @param {Object} country
+ * @return {Object} this
+ */
+WorldMap.prototype._clickCountry = function(country) {
+	if (country.id !== this.selected) {
+		this.selected = country.id;
+		country.style.fill = '#F00';
+		return this;
+	}
+
+	this.selected = null;
+	country.style.fill = '#000';
+	return this;
+};
+
+/**
+ * Update Caption
+ * @return {Object} this
+ */
+ WorldMap.prototype._updateCaption = function() {
+ 	var caption = document.querySelector('.caption');
+ 	var captionList = document.createElement('ul');
+ 	var listItem = document.createElement('li');
+
+ 	caption.innerHTML = '';
+ 	if (this.selected) {
+ 		var country = this.map.querySelector('#' + this.selected);
+ 		var countryName = country.id;
+ 		var firstLetter = countryName.charAt(0).toUpperCase();
+
+ 		countryName = firstLetter + countryName.slice(1);
+ 		listItem.textContent = countryName;
+ 		captionList.appendChild(listItem);
+ 		caption.appendChild(captionList);
+ 	}
+ };
 
 /* Test */
 var worldMap = new WorldMap();
