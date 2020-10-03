226094a471b2c2328e7f10063f55c3ce8f8205be
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-10-03 17:23:03 +0200

    feat(placeholder): add animation for placeholder

--- exercise-5/index.js
+++ exercise-5/index.js
@@ -9,7 +9,61 @@ var Form = function Form(form) {
 	this.inputs = form.querySelectorAll('input');
 };
 
+/**
+ * Run
+ * @return {Object} this
+ */
+Form.prototype.run = function() {
+	this._bindEvents();
+	return this;
+};
+
+/**
+ * Bind Events
+ * @return {Object} this
+ */
+Form.prototype._bindEvents = function() {
+	this.inputs.forEach(function(input) {
+		input.addEventListener('focus', function(event) {
+			var parentNode = event.target.parentNode;
+			var placeholder = parentNode.querySelector('.input-placeholder');
+
+			event.target.placeholder = '';
+			placeholder.style.opacity = '1';
+			placeholder.style.visibility = 'visible';
+		}.bind(this));
+		input.addEventListener('blur', function(event) {
+			var parentNode = event.target.parentNode;
+			var placeholder = parentNode.querySelector('.input-placeholder');
+
+			if (!event.target.value) {
+				event.target.placeholder = placeholder.textContent;
+				placeholder.style.opacity = '0';
+				placeholder.style.visibility = 'hidden';
+			}
+
+		}.bind(this));
+		input.addEventListener('input', function(event) {
+			var parentNode = event.target.parentNode;
+			var placeholder = parentNode.querySelector('.input-placeholder');
+
+			if (event.target.value) {
+				placeholder.style.fontSize = '0.7em';
+				placeholder.style.transform = 'translateY(0)';
+			}
+
+			if (!event.target.value) {
+				placeholder.style.fontSize = '1em';
+				placeholder.style.transform = 'translateY(-185%)';
+			}
+
+		}.bind(this));
+	})
+	return this;
+};
+
 /* Test */
 var element = document.querySelector('form');
 var form = new Form(element);
+form.run();
 /* End Test */
