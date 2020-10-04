8c28d4de617aa88b326d5d804625386ea4a29943
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-10-04 15:08:49 +0200

    feat(checkEmptyFields): add method checking if fields are empty

--- exercise-5/index.js
+++ exercise-5/index.js
@@ -7,6 +7,8 @@
 var Form = function Form(form) {
 	this.form = form;
 	this.inputs = form.querySelectorAll('input');
+	this.submit = form.querySelector('button');
+	this.isValid = true;
 };
 
 /**
@@ -28,6 +30,7 @@ Form.prototype._bindEvents = function() {
 			var parentNode = event.target.parentNode;
 			var placeholder = parentNode.querySelector('.input-placeholder');
 
+			event.target.classList.add('error');
 			event.target.placeholder = '';
 			placeholder.style.opacity = '1';
 			placeholder.style.visibility = 'visible';
@@ -35,11 +38,15 @@ Form.prototype._bindEvents = function() {
 		input.addEventListener('blur', function(event) {
 			var parentNode = event.target.parentNode;
 			var placeholder = parentNode.querySelector('.input-placeholder');
+			var error = parentNode.querySelector('.input-error');
 
+			error.textContent = '';
 			if (!event.target.value) {
 				event.target.placeholder = placeholder.textContent;
+				event.target.classList.add('error');
 				placeholder.style.opacity = '0';
 				placeholder.style.visibility = 'hidden';
+				error.textContent = "Ce champ est requis";
 			}
 
 		}.bind(this));
@@ -58,7 +65,34 @@ Form.prototype._bindEvents = function() {
 			}
 
 		}.bind(this));
-	})
+	});
+	this.form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		this._checkEmptyFields();
+
+		if (this.isValid) {
+			event.target.submit();
+		}
+
+	}.bind(this));
+	return this;
+};
+
+/**
+ * Check Empty Fields
+ * @return {Object} this
+ */
+Form.prototype._checkEmptyFields = function() {
+	this.inputs.forEach(function(input) {
+		var parentNode = input.parentNode;
+
+		if (!input.value) {
+			this.isValid = false;
+			parentNode.querySelector('.input-error').textContent = 'Ce champ est requis';
+			input.classList.add('error');
+		}
+
+	}.bind(this));
 	return this;
 };
 
