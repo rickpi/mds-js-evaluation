8a49b56ae155f3db4cbf318e264afdd293cb5cf2
Pierrick MERLANDE <merlandepierrick@gmail.com>
2020-10-09 22:53:28 +0200

    feat(computeNotes): add function computing notes average

--- exercise-7/index.js
+++ exercise-7/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+/**
+ * Compute Notes
+ * @param {number[]} notes
+ * @return {number} average
+ */ 
+var computeNotes = function computeNotes(notes) {
+	var amount = 0;
+	var average;
+
+	notes.forEach(function(note) {
+		amount += note;
+	});
+	return amount / notes.length;
+}
+
+console.log(computeNotes([20, 17, 18, 19]));
